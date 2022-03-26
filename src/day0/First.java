43bc7286c538ace9e6a584b3fe7a1ce588b94bb0
ozkanerenq <ozkannerenn@gmail.com>
2022-03-26 16:10:21 -0400

    wrote the code for First class

---
+++ src/day0/First.java
@@ -0,0 +1,14 @@
+package day0;
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("Working...");
+    }
+
+    //extra line 1
+    //extra line 2
+    //extra line 3
+
+}
